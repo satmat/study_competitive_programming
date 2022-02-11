5406efd6bdd2372db12fa34b5e2737f66dd29bd1
satmat <satmat@tg8.so-net.ne.jp>
2022-02-11 23:39:23 +0900

    add

---
+++ p72.cpp
@@ -0,0 +1,11 @@
+// 競技プログラミングのためのアルゴリズムとデータ構造
+// p.072
+
+#include<iostream>
+using namespace std;
+
+struct Card { char suit, value;}
+
+void bubble(struct Card A[], int N) {
+    for ( int i =  )
+}
